995c7d3f9fa8b8f42d79e46670222998d90d0a5e
minyugyeong <alsdbrud123@naver.com>
2024-02-05 18:34:52 +0900

    섹션3 - 객체 지향 프로그래밍

---
+++ src/oop1/ex/RectangleOopMain.java
@@ -0,0 +1,22 @@
+package oop1.ex;
+
+public class RectangleOopMain {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.height = 10;
+        rectangle.width = 10;
+
+        int area = rectangle.calculateArea();
+        System.out.println("넓이: " + area);
+
+        int perimeter = rectangle.calculatePerimeter();
+        System.out.println("둘레 길이: " + perimeter);
+
+        boolean square = rectangle.isSquare();
+        System.out.println("정사각형 여부: " + square);
+    }
+
+
+}
+
+
