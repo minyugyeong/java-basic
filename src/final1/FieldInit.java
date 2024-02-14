50b0344da04f6d33a029f8dbcb95eea28af06418
minyugyeong <alsdbrud123@naver.com>
2024-02-14 18:04:01 +0900

    섹션8 - final(final 변수와 상수1)

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+    
+//    public FieldInit(int value){ //컴파일 오류
+//        this.value = value;
+//    }
+}
