045d47c0508582accfb114f805a4e9cfa66215ff
minyugyeong <alsdbrud123@naver.com>
2024-02-19 19:37:05 +0900

    섹션11 - 다형성2(다형성 활용3)

---
+++ src/ploy/ex2/Duck.java
@@ -0,0 +1,9 @@
+package ploy.ex2;
+
+public class Duck extends Animal {
+    
+    @Override
+    public void sound(){
+        System.out.println("꽥꽥");
+    }
+}
---
+++ src/ploy/ex2/Pig.java
@@ -0,0 +1,5 @@
+package ploy.ex2;
+
+public class Pig extends Animal {
+    //sound 해야 하는데..
+}
